f22288ccdec607074c8ad35b8393884264b4b4b9
JamesBrian1 <71192827+JamesBrian1@users.noreply.github.com>
2020-09-12 16:47:59 -0400

    Update Main.java
    Changed everyone to developers in welcome message

--- src/com/heartbeat/Main.java
+++ src/com/heartbeat/Main.java
@@ -4,6 +4,6 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-        System.out.println("Welcome everyone!");
+        System.out.println("Welcome developers!");
     }
 }
